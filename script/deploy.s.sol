6bf34aba0400c14815b8db9d11d09eb62ecdc43b
Raezil <kamil.mazurek319@gmail.com>
2025-08-06 17:45:59 +0200

    feat: adding script/deploy.s.sol

---
+++ script/deploy.s.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import {Marketplace}    from "../src/marketplace.sol";
+import {Interactor}     from "../src/interactor.sol";
+import {Token}          from "../src/contract.sol";
+
+contract DeployAll is Script {
+    function run() external {
+        // begin broadcasting to the network
+        vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
+
+        // 1. Deploy Token
+        Token token = new Token();
+
+        // 2. Deploy Marketplace with the Token address
+        Marketplace marketplace = new Marketplace(address(token));
+
+        // 3. Deploy Interactor with the Marketplace address
+        Interactor interactor = new Interactor(address(marketplace));
+        console.log("Interactor deployed at:", address(interactor));
+        // stop broadcasting
+        vm.stopBroadcast();
+    }
+}
